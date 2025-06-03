4c79393b07252974dcc0da7352de4b8ee0d37cd3
DisasterArea96 <gsalomon@zoho.com>
2025-06-03 23:40:43 +0100

    Updated version number

--- src/data/dream_strings.h
+++ src/data/dream_strings.h
@@ -1,5 +1,5 @@
 // Version strings for dream factory
-const u8 gText_DreamFactoryVersion[] = _("Dream Factory - v2.1");
+const u8 gText_DreamFactoryVersion[] = _("Dream Factory - v2.2");
 
 const u8 gText_DreamFactoryVersionSpacer[] = _("");
 const u8 gText_DreamFactoryVersionPrefix[] = _("");
